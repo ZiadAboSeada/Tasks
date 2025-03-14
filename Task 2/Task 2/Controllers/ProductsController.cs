694841479e75c4f73d066d976d4c241cc005e65a
Ziad Muhamed <Ziad.Ibrahim.FCI22284@sadatacademy.edu.eg>
2025-03-14 05:21:47 +0200

    first commit

---
+++ Task 2/Task 2/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Task_2.Models;
+
+namespace Task_2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        ApplicationDBContext _context = new ApplicationDBContext();
+
+        public IActionResult Index()
+        {
+
+            var products = _context.products.ToList();
+            ViewData["prod"] = products;
+            return View("Index");
+
+        }
+
+
+        public IActionResult Details(int id)
+        {
+            Product products = _context.products.FirstOrDefault(c => c.Id == id);
+            ViewBag.Product = products;
+            return View();
+        }
+
+    }
+}
---
+++ Task 2/Task 2/ViewModel/OrderwithCustomer.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_2.ViewModel
+{
+    public class OrderwithCustomer
+    {
+        public int OrderId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerPhone { get; set; }
+
+
+    }
+}
