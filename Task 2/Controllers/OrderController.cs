d09f3c6aa5948c9ff1fa62ff42b0b2e9900f4ee8
Youssif Mohamed <youssif.mohamed@qarartech.com>
2025-04-17 17:10:56 +0200

    task3

---
+++ Task 2/Controllers/OrderController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Task_2.Models;
+using Task_2.ViewModel;
+using static NuGet.Packaging.PackagingConstants;
+
+namespace Task_2.Controllers
+{
+    public class OrderController : Controller
+    {
+        ApplicationDBContext _context = new ApplicationDBContext();
+        public IActionResult Index()
+        {
+            var order = _context.orders.Include(e => e.Customer).ToList();
+            List<OrderwithCustomer> OrderVM = new List<OrderwithCustomer>();
+
+            foreach (var item in order)
+            {
+                OrderVM.Add(new OrderwithCustomer
+                {
+                    OrderId = item.Id,
+                    CustomerName = item.Customer.Name,
+                    CustomerPhone = item.Customer.Phone,
+                    CustomerEmail = item.Customer.Email
+                });
+            }
+            return View(OrderVM);
+
+        }
+        public IActionResult Details(int id)
+        {
+            var order2 = _context.orders
+                        .Include(o => o.orderItems)
+                        .ThenInclude(oi => oi.product)
+                        .FirstOrDefault(o => o.Id == id);
+
+            OrderWithItemsProducts OrdersVM = new OrderWithItemsProducts();
+            OrdersVM.OrderId = order2.Id;
+            OrdersVM.OrderCustomerId = order2.CustomerId;
+            OrdersVM.OrderDate = order2.OrderDat;
+            OrdersVM.OrderTotalAmount = order2.TotalAmount;
+            OrdersVM.ItemId =  order2.orderItems.First().Id;
+            OrdersVM.ItemQuantity = order2.orderItems.First().Quantity;
+            OrdersVM.ItemPrice = order2.orderItems.First().price;
+            OrdersVM.ProductId = order2.orderItems.First().product.Id;
+            OrdersVM.ProductName = order2.orderItems.First().product.Name;
+            OrdersVM.ProductDescription = order2.orderItems.First().product.Description;
+            OrdersVM.ProductCategoryId = order2.orderItems.First().product.CategoryId;
+            OrdersVM.ProductPrice = order2.orderItems.First().product.price;
+          
+          
+            return View(OrdersVM);
+        }
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+        public IActionResult save(Order new_ord)
+        {
+            _context.orders.Add(new_ord);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult delete(int id)
+        {
+            var ord = _context.orders.FirstOrDefault(o => o.Id == id);
+            _context.orders.Remove(ord);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult UpdateForm (int id)
+        {
+            var ord = _context.orders.FirstOrDefault(O=> O.Id == id);
+            return View("Update",ord);
+        }
+        public IActionResult Edit(int id, Order OrdReq)
+        {
+            var ord = _context.orders.FirstOrDefault(o => o.Id == id);
+            ord.Id = OrdReq.Id;
+            ord.TotalAmount = OrdReq.TotalAmount;
+            return RedirectToAction("index");
+        }
+        
+    }
+}
---
+++ Task 2/Controllers/ProductsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Task_2.Models;
+
+namespace Task_2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        ApplicationDBContext _context = new ApplicationDBContext();
+
+        public IActionResult Index()
+        {
+
+            var products = _context.products.ToList();
+            ViewData["prod"] = products;
+            return View("Index");
+
+        }
+
+
+        public IActionResult Details(int id)
+        {
+            Product products = _context.products.FirstOrDefault(c => c.Id == id);
+            ViewBag.Product = products;
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+        public IActionResult Save(Product newproduct)
+        {
+            _context.products.Add(newproduct);
+            _context.SaveChanges();
+            return RedirectToAction("index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var product = _context.products.FirstOrDefault(p => p.Id == id);
+            _context.products.Remove(product);
+            _context.SaveChanges();
+            return RedirectToAction("index");
+        }
+        public IActionResult UpdateForm(int id)
+        {
+            var Pr = _context.products.FirstOrDefault(c => c.Id == id);
+            return View("update", Pr);
+        }
+        public ActionResult Edit(int id, Product PrReq)
+        {
+            var Prdb = _context.products.FirstOrDefault(c => c.Id == id);
+            Prdb.Name = PrReq.Name;
+            Prdb.Description = PrReq.Description;
+            Prdb.price = PrReq.price;
+            Prdb.Img = PrReq.Img;
+            Prdb.CategoryId = PrReq.CategoryId;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
